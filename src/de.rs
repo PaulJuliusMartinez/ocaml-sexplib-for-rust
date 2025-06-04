c48caf8c07a3591f9d291ef92122b74ca44e4537
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2025-06-03 21:32:23 -0400

    Add deserialize implementations for char, strings, options, unit, unit struct, newtype structs, tuples and tuple structs.

--- src/de.rs
+++ src/de.rs
@@ -11,8 +11,12 @@ use serde::Deserialize;
 use crate::error::{Error, Result};
 use crate::Sexp;
 
-fn error<V>(f: &str) -> Result<V> {
-    Err(Error::DeserializationError(f.to_string()))
+fn error<V>(s: &str) -> Result<V> {
+    Err(Error::DeserializationError(s.to_string()))
+}
+
+fn error_string<V>(s: String) -> Result<V> {
+    Err(Error::DeserializationError(s))
 }
 
 fn parse_int_error(err: ParseIntError) -> Error {
@@ -23,6 +27,7 @@ fn parse_float_error(err: ParseFloatError) -> Error {
     Error::DeserializationError(format!("unable to parse float: {:#?}", err))
 }
 
+#[derive(Debug)]
 pub struct Deserializer<'a: 'de, 'de> {
     input_sexp: &'de Sexp<'a>,
     cursor: Vec<(&'de [Sexp<'a>], usize)>,
@@ -41,7 +46,7 @@ impl<'a: 'de, 'de> Deserializer<'a, 'de> {
         }
     }
 
-    fn next_atom<'b>(&'b mut self) -> Result<&'b Cow<'a, str>> {
+    fn next_atom(&mut self) -> Result<&'de Cow<'a, str>> {
         let cursor = self.cursor.last_mut().unwrap();
         match cursor.0.get(cursor.1) {
             None => error("exhausted current input"),
@@ -73,17 +78,31 @@ impl<'a: 'de, 'de> Deserializer<'a, 'de> {
             .map_err(parse_float_error)
     }
 
-    fn start_deserializing_seq<'b>(&'b mut self) -> Result<SeqDeserializer<'a, 'de, 'b>> {
-        let (sexps, index) = self.cursor.last().unwrap();
-        match sexps.get(*index) {
+    fn step_into_list(&mut self) -> Result<()> {
+        let cursor = self.cursor.last_mut().unwrap();
+        match cursor.0.get(cursor.1) {
             None => return error("exhaused current input"),
-            Some(Sexp::Atom(_)) => return error("expected list"),
+            Some(Sexp::Atom(_)) => error("expected list"),
             Some(Sexp::List(list)) => {
+                cursor.1 += 1;
                 self.cursor.push((list.as_slice(), 0));
+                Ok(())
             }
         }
+    }
 
+    fn step_out_of_list(&mut self) {
+        self.cursor.pop();
+    }
+
+    fn start_deserializing_seq<'b>(&'b mut self) -> Result<SeqDeserializer<'a, 'de, 'b>> {
+        self.step_into_list()?;
         Ok(SeqDeserializer(self))
+    }
+
+    fn curr_list_len(&self) -> usize {
+        let (sexps, _) = self.cursor.last().unwrap();
+        sexps.len()
     }
 
     fn have_more_elems_to_process(&self) -> bool {
@@ -161,25 +180,35 @@ impl<'a: 'de + 'b, 'de: 'b, 'b> de::Deserializer<'de> for &'b mut Deserializer<'
     impl_deserialize_float!(deserialize_f32, visit_f32);
     impl_deserialize_float!(deserialize_f64, visit_f64);
 
-    fn deserialize_char<V>(self, _visitor: V) -> Result<V::Value>
+    fn deserialize_char<V>(self, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        error("`deserialize_char` not implemented yet")
+        let mut chars = self.next_atom()?.chars();
+        match chars.next() {
+            None => error("Expected char but got empty string"),
+            Some(ch) => {
+                if chars.next().is_some() {
+                    error("Expected single char but got multi-char string")
+                } else {
+                    visitor.visit_char(ch)
+                }
+            }
+        }
     }
 
-    fn deserialize_str<V>(self, _visitor: V) -> Result<V::Value>
+    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        error("`deserialize_str` not implemented yet")
+        visitor.visit_borrowed_str(self.next_atom()?.as_ref())
     }
 
-    fn deserialize_string<V>(self, _visitor: V) -> Result<V::Value>
+    fn deserialize_string<V>(self, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        error("`deserialize_string` not implemented yet")
+        visitor.visit_borrowed_str(self.next_atom()?.as_ref())
     }
 
     fn deserialize_bytes<V>(self, _visitor: V) -> Result<V::Value>
@@ -196,32 +225,49 @@ impl<'a: 'de + 'b, 'de: 'b, 'b> de::Deserializer<'de> for &'b mut Deserializer<'
         error("`deserialize_byte_buf` not implemented yet")
     }
 
-    fn deserialize_option<V>(self, _visitor: V) -> Result<V::Value>
+    fn deserialize_option<V>(self, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        error("`deserialize_option` not implemented yet")
+        self.step_into_list()?;
+        match self.curr_list_len() {
+            0 => visitor.visit_none(),
+            1 => visitor.visit_some(self),
+            _ => error("Expected Some value for option (single-element-list), but got list with multiple elements"),
+        }
     }
 
-    fn deserialize_unit<V>(self, _visitor: V) -> Result<V::Value>
+    fn deserialize_unit<V>(self, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        error("`deserialize_unit` not implemented yet")
+        self.step_into_list()?;
+        match self.curr_list_len() {
+            0 => {
+                self.step_out_of_list();
+                visitor.visit_unit()
+            }
+            _ => error("Expected empty list for (), i.e,. unit, but list contained values"),
+        }
     }
 
-    fn deserialize_unit_struct<V>(self, _name: &'static str, _visitor: V) -> Result<V::Value>
+    fn deserialize_unit_struct<V>(self, name: &'static str, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        error("`deserialize_unit_struct` not implemented yet")
+        let atom = self.next_atom()?;
+        if atom.as_ref() == name {
+            visitor.visit_unit()
+        } else {
+            error_string(format!("Expected atom: {:?}, got: {:?}", name, atom))
+        }
     }
 
-    fn deserialize_newtype_struct<V>(self, _name: &'static str, _visitor: V) -> Result<V::Value>
+    fn deserialize_newtype_struct<V>(self, _name: &'static str, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        error("`deserialize_newtype_struct` not implemented yet")
+        visitor.visit_newtype_struct(self)
     }
 
     fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value>
@@ -234,23 +280,36 @@ impl<'a: 'de + 'b, 'de: 'b, 'b> de::Deserializer<'de> for &'b mut Deserializer<'
         Ok(value)
     }
 
-    fn deserialize_tuple<V>(self, _len: usize, _visitor: V) -> Result<V::Value>
+    fn deserialize_tuple<V>(self, len: usize, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        error("`deserialize_tuple` not implemented yet")
+        let seq_access = self.start_deserializing_seq()?;
+        // Someday: This is silly that I have to access self via `seq_access.0`...
+        // Maybe this goes away if `seq_access` is improved?
+        let curr_list_len = seq_access.0.curr_list_len();
+        if curr_list_len != len {
+            error_string(format!(
+                "Expected list of length {len} for tuple, but got list of length {curr_list_len}"
+            ))
+        } else {
+            let value = visitor.visit_seq(seq_access)?;
+            // Someday: Make sure we've processed all of the elements?
+            // Do we still need to do this with the `curr_list_len` check?
+            Ok(value)
+        }
     }
 
     fn deserialize_tuple_struct<V>(
         self,
         _name: &'static str,
-        _len: usize,
-        _visitor: V,
+        len: usize,
+        visitor: V,
     ) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        error("`deserialize_tuple_struct` not implemented yet")
+        self.deserialize_tuple(len, visitor)
     }
 
     fn deserialize_map<V>(self, _visitor: V) -> Result<V::Value>
@@ -318,6 +377,8 @@ impl<'a: 'de + 'b, 'de: 'b, 'b> SeqAccess<'de> for SeqDeserializer<'a, 'de, 'b> 
 mod tests {
     use super::*;
 
+    use insta::assert_debug_snapshot;
+
     fn a(s: &str) -> Sexp {
         Sexp::Atom(Cow::from(s))
     }
@@ -348,6 +409,121 @@ mod tests {
     }
 
     #[test]
+    fn test_char() {
+        assert_eq!('a', from_sexp(&a("a")).unwrap());
+        assert_eq!('α', from_sexp(&a("α")).unwrap());
+
+        assert_debug_snapshot!(from_sexp::<char>(&a("")), @r#"
+        Err(
+            DeserializationError(
+                "Expected char but got empty string",
+            ),
+        )
+        "#);
+
+        assert_debug_snapshot!(from_sexp::<char>(&a("abc")), @r#"
+        Err(
+            DeserializationError(
+                "Expected single char but got multi-char string",
+            ),
+        )
+        "#);
+    }
+
+    #[test]
+    fn test_strings() {
+        assert_eq!("abc", from_sexp::<&str>(&a("abc")).unwrap());
+        assert_eq!("123", from_sexp::<String>(&a("123")).unwrap());
+    }
+
+    #[test]
+    fn test_option() {
+        assert_eq!(None, from_sexp::<Option<i32>>(&l(vec![])).unwrap());
+        assert_eq!(Some(3), from_sexp::<Option<i32>>(&l(vec![a("3")])).unwrap());
+
+        assert_debug_snapshot!(from_sexp::<Option<i32>>(&a("1")), @r#"
+        Err(
+            DeserializationError(
+                "expected list",
+            ),
+        )
+        "#);
+
+        assert_debug_snapshot!(from_sexp::<Option<i32>>(&l(vec![a("1"), a("2")])), @r#"
+        Err(
+            DeserializationError(
+                "Expected Some value for option (single-element-list), but got list with multiple elements",
+            ),
+        )
+        "#);
+    }
+
+    #[test]
+    fn test_unit() {
+        assert_eq!((), from_sexp::<()>(&l(vec![])).unwrap());
+
+        assert_debug_snapshot!(from_sexp::<()>(&a("unit")), @r#"
+        Err(
+            DeserializationError(
+                "expected list",
+            ),
+        )
+        "#);
+
+        assert_debug_snapshot!(from_sexp::<()>(&l(vec![a("unit")])), @r#"
+        Err(
+            DeserializationError(
+                "Expected empty list for (), i.e,. unit, but list contained values",
+            ),
+        )
+        "#);
+    }
+
+    #[test]
+    fn test_unit_struct() {
+        #[derive(Debug, Deserialize, PartialEq, Eq)]
+        struct UnitStruct;
+        assert_eq!(
+            UnitStruct,
+            from_sexp::<UnitStruct>(&a("UnitStruct")).unwrap()
+        );
+
+        assert_debug_snapshot!(from_sexp::<UnitStruct>(&a("unit_struct")), @r#"
+        Err(
+            DeserializationError(
+                "Expected atom: \"UnitStruct\", got: \"unit_struct\"",
+            ),
+        )
+        "#);
+
+        assert_debug_snapshot!(from_sexp::<UnitStruct>(&l(vec![])), @r#"
+        Err(
+            DeserializationError(
+                "expected atom",
+            ),
+        )
+        "#);
+    }
+
+    #[test]
+    fn test_newtype_struct() {
+        #[derive(Debug, Deserialize, PartialEq, Eq)]
+        struct NewtypeStruct(u8);
+        assert_eq!(
+            NewtypeStruct(1),
+            from_sexp::<NewtypeStruct>(&a("1")).unwrap()
+        );
+
+        assert_debug_snapshot!(from_sexp::<NewtypeStruct>(&l(vec![a("NewtypeStruct"), a("1")])), @r#"
+        Err(
+            DeserializationError(
+                "expected atom",
+            ),
+        )
+        "#);
+    }
+
+    #[test]
     fn test_seq() {
         assert_eq!(
             vec![true, false],
@@ -358,5 +534,41 @@ mod tests {
             vec![1u8, 2u8],
             from_sexp::<Vec<u8>>(&l(vec![a("1"), a("2")])).unwrap()
         );
+
+        assert_eq!(
+            vec![(), (), ()],
+            from_sexp::<Vec<()>>(&l(vec![l(vec![]), l(vec![]), l(vec![])])).unwrap()
+        );
+    }
+
+    #[test]
+    fn test_tuple_and_tuple_struct() {
+        #[derive(Debug, Deserialize, PartialEq, Eq)]
+        struct TupleStruct<'a>(i8, bool, &'a str);
+
+        assert_eq!(
+            (1, true, "abc"),
+            from_sexp::<(i8, bool, &str)>(&l(vec![a("1"), a("true"), a("abc")])).unwrap(),
+        );
+        assert_eq!(
+            TupleStruct(1, true, "abc"),
+            from_sexp::<TupleStruct>(&l(vec![a("1"), a("true"), a("abc")])).unwrap(),
+        );
+
+        assert_debug_snapshot!(from_sexp::<(i8, bool, String)>(&l(vec![a("0"), a("false")])), @r#"
+        Err(
+            DeserializationError(
+                "Expected list of length 3 for tuple, but got list of length 2",
+            ),
+        )
+        "#);
+
+        assert_debug_snapshot!(from_sexp::<TupleStruct>(&l(vec![a("0"), a("false")])), @r#"
+        Err(
+            DeserializationError(
+                "Expected list of length 3 for tuple, but got list of length 2",
+            ),
+        )
+        "#);
     }
 }
